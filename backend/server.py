b18e0b911f6d74fcaafbb9674941d9a1d121afa5
Kwiasek <kwiasekkwiasek@gmail.com>
2026-02-27 18:31:09 +0100

    Server multithreading

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template_string, redirect, request, jsonify
 from flask_cors import CORS
 import subprocess
 import os
+import threading
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,9 +11,59 @@ CATT_CMD = os.environ.get("CATT_CMD", "catt")
 YTDLP_CMD = os.environ.get("YTDLP_CMD", "yt-dlp")
 PORT = int(os.environ.get("FLASK_PORT", 8080))
 
-app = Flask(__name__, static_folder='static', static_url_path='/')
-
+app = Flask(__name__)
 CORS(app)   
+
+current_ytdlp_process = None
+cast_lock = threading.Lock()
+
+def background_cast_task(url):
+    global current_ytdlp_process
+
+    with cast_lock:
+        if current_ytdlp_process is not None:
+            print("[THREAD] New request! Killing last yt-dlp process...")
+            try:
+                current_ytdlp_process.terminate()
+            except Exception:
+                pass
+            current_ytdlp_process = None
+    
+    print(f"[THREAD] Parsing link: {url}")
+
+    try:
+        process = subprocess.Popen(
+            [YTDLP_CMD, "-g", "-f", "best", url],
+            stdout=subprocess.PIPE
+            stderr=subprocess.PIPE
+        )
+
+        with cast_lock:
+            current_ytdlp_process = process
+
+        stdout, stderr = process.communicate()
+
+        with cast_lock:
+            if current_ytdlp_process != process:
+                print("[THREAD] This thread was terminated by new one. Canceling casting.")
+                return
+            else:
+                current_ytdlp_process = None
+        
+        if process.returncode == 0:
+            direct_url = stdout.decode('utf-8').strip()
+            if direct_url:
+                print("[THREAD] Got Link! Casting...")
+                subprocess.Popen([CATT_CMD, "cast", direct_url])
+            else:
+                print(f"[THREAD] Error yt-dlp: {stderr.decode('utf-8')}")
+
+    except Exception as e:
+        print("[THREAD] Error while casting in background: {e}")
+        with cast_lock:
+            if current_ytdlp_process == process:
+                current_ytdlp_process = None
+
 
 @app.route('/api/status')
 def check_status():
@@ -32,15 +83,11 @@ def cast_video():
     url = data.get('video_url', '').strip() if data else ''
     
     if url:
-        try:
-            direct_url = subprocess.check_output([YTDLP_CMD, "-g", "-f", "best", url]).decode('utf-8').strip()
-            if direct_url:
-                subprocess.Popen([CATT_CMD, "cast", direct_url])
-                return jsonify({"message": "Casting Video"}), 200
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
+        thread = threading.Thread(target=background_cast_task, args=(url,))
+        thread.start()
+        return jsonify({"message": "Parsing video in background..."}), 200
             
-    return jsonify({"error": "Missing link"}), 400
+    return jsonify({"error": "No link"}), 400
 
 @app.route('/api/cmd/<action>')
 def execute_command(action):
